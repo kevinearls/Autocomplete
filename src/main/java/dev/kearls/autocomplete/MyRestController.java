cd41625e0213ef861f1430df3fed60333b52421f
Kevin Earls <kevinearls@mac.com>
2024-12-30 18:43:20 +0100

    Mostly working, check TODOs and FIXMEs
    Signed-off-by: Kevin Earls <kevinearls@mac.com>

--- src/main/java/dev/kearls/autocomplete/MyRestController.java
+++ src/main/java/dev/kearls/autocomplete/MyRestController.java
@@ -1,76 +1,70 @@
 package dev.kearls.autocomplete;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import java.io.IOException;
-import java.net.URI;
-import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1")
 public class MyRestController {
-//    private final HttpClient httpClient = HttpClient.newBuilder()
-//            .version(HttpClient.Version.HTTP_2)
-//            .build();
-//    private static final String API_KEY = System.getenv("GOOGLE_MAPS_API_KEY");
+    private final HttpClient httpClient = HttpClient.newBuilder()
+            .version(HttpClient.Version.HTTP_2)
+            .build();
+    private static final String API_KEY = System.getenv("GOOGLE_MAPS_API_KEY");
+    ObjectMapper objectMapper = new ObjectMapper();
 
+    /**
+     * NOTE: I *think* we should be able to get this from the Google Maps Places API
+     * (https://developers.google.com/maps/documentation/places/web-service/autocomplete)
+     * but so far I haven't been able to get that to work.
+     *
+     * @param partial
+     * @return
+     */
     @GetMapping("/{partial}")
     public List<String> auto(@PathVariable String partial) {
+        // FIXME build a cache!
+        var theURI = UriComponentsBuilder.newInstance()
+                .scheme("https")
+                .host("maps.googleapis.com")
+                .pathSegment("maps", "api", "place", "autocomplete", "json")
+                .queryParam("input", partial)
+                .queryParam("types", "geocode")
+                .queryParam("key", API_KEY)
+                .build()
+                .toUri();
 
-        var places = List.of("Ipswich, UK"
-                , "Ipswich QLD, Australia"
-                , "Ipswich, MA, USA"
-                , "Ipswich Train Station, A137, Ipswich, UK"
-                , "Ipswich, SD, USA");
-        System.out.println(">>>>> Partial was " + partial);
-        // "https://maps.googleapis.com/maps/api/place/autocomplete/json?input=Ipswich&types=geocode&key=$GOOGLE_MAPS_API_KEY"
+        HttpRequest request = HttpRequest.newBuilder()
+                .GET()
+                .uri(theURI)
+                .setHeader("User-Agent", "Java 11 HttpClient Bot")  // FIXME do we need this?
+                .build();
 
+        try {
+            HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+            System.out.println(response.statusCode());  // FIXME check this and log an error if not 2xx
 
-//        var theURI = UriComponentsBuilder.newInstance()
-//                .scheme("https")
-//                .host("maps.googleapis.com")
-//                .path("maps")
-//                .path("api")
-//                .path("place")
-//                .path("autocomplete")
-//                .path("json")
-//                .queryParam("input", "Ipswich")  // this is where the input will go
-//                .queryParam("types", "geocode")
-//                .queryParam("key", "API_KEY goes here")
-//                .build()
-//                .toUri();
-//
-//        System.out.println("URI: "  + theURI.toString());
+            JsonNode arrayNode = objectMapper.readTree(response.body());
+            JsonNode predictions = arrayNode.get("predictions");
 
-//        HttpRequest request = HttpRequest.newBuilder()
-//                .GET()
-//                .uri(theURI)
-//                .setHeader("User-Agent", "Java 11 HttpClient Bot")
-//                .build();
-//
-//        HttpResponse<String> response = null;
-//        try {
-//            response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
-//        } catch (IOException e) {
-//            throw new RuntimeException(e);
-//        } catch (InterruptedException e) {
-//            throw new RuntimeException(e);
-//        }
-//
-//        // print status code
-//        System.out.println(response.statusCode());
-//
-//        // print response body
-//        System.out.println(response.body());
-
-        return places;
+            var placeNames = new ArrayList<String>();
+            for (JsonNode prediction : predictions) {
+                placeNames.add(prediction.get("description").textValue());
+            }
+            return placeNames;
+        } catch (Exception e) {
+            // FIXME log an error, don't throw an exception
+            throw new RuntimeException(e);
+        }
     }
 }
